710f6e0220fcd74c6508013d16a0ecf67436de73
rahwee <rahweekh@gmail.com>
2024-07-02 23:56:52 +0700

    implement tour model

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,18 +1,8 @@
-const fs = require('fs');
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
 
 
-exports.checkID = (req, res, next, val) => {
-    if(req.params.id * 1 > tours.length ) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
-    next();
-};
+const tour = require('./../models/tourModel')
+
+
 
 exports.checkBodyParam = (req, res, next) => {
   if(!req.body.name || !req.body.price){
--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const app = require('./app');
-
-
 require('dotenv').config();
+const mongoose = require('mongoose');
+
 const port = process.env.PORT || 3000;
+
+// Database connection URL with password replacement
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+// connect ot mongoose db
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(con => {
+  // console.log(con.connections)
+  console.log('DB connection successfully')
+});
+
+
 
 
 app.listen(port, () => {
